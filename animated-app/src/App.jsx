6a092c532babe19f16077d603e1b7b8fb8b670af
tirumalateja19 <tirumalateja.jampani@gmail.com>
2025-10-01 11:14:20 +0530

    Tailwind setUp in CRA

---
+++ animated-app/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-white">
+      <h1 className="text-xl font-bold underline ">Tailwind set-up with CRA</h1>
+    </div>
+  );
+};
+
+export default App;
