15bffbae0a452d0eb8a6ae0f566b1378f109b72f
tirumalateja19 <tirumalateja.jampani@gmail.com>
2026-05-25 17:12:01 +0530

    timelines & position parameters

--- gsap-practice/index.js
+++ gsap-practice/index.js
@@ -1,4 +1,4 @@
-gsap.defaults({ duration: 2, delay: 1, ease: "power4.inOut" });
+gsap.defaults({ duration: 2, ease: "power4.inOut" });
 
 gsap.from(".name", {
   x: -500,
@@ -9,18 +9,15 @@ gsap.from(".name2", {
   opacity: 0,
 });
 
-let tween = gsap.to(
-  ".box",
-  {
-    x: 900,
-    opacity: 1,
-    duration: 6,
-    ease: "power2.inOut",
-    rotation: 360,
-    // repeat: 1,
-    onComplete: () => console.log("Tween completed"),
-  },
-);
+let tween = gsap.to(".box", {
+  x: 900,
+  opacity: 1,
+  duration: 6,
+  ease: "power2.inOut",
+  rotation: 360,
+  // repeat: 1,
+  onComplete: () => console.log("Tween completed"),
+});
 
 document.querySelector("#play").onclick = () => tween.play();
 document.querySelector("#pause").onclick = () => tween.pause();
@@ -36,3 +33,8 @@ const tl = gsap.timeline();
 tl.to(".step1", { opacity: 0, y: -20, duration: 0.5 });
 tl.to(".step2", { opacity: 0, y: -20, duration: 0.5 });
 tl.to(".step3", { opacity: 0, y: -20, duration: 0.5 });
+const tl2 = gsap.timeline();
+tl2.to("#tl2-a", { width: "100%", duration: 0.8 });
+tl2.to("#tl2-b", { width: "100%", duration: 0.8 }, "<=0.3");
+tl2.to("#tl2-c", { width: "100%", duration: 0.8 }, "<=0.4");
+
